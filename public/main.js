ea2d654509565ecdb3dfab55553e73330a516fd3
fccJashCoda <personalrevolution2014@gmail.com>
2021-01-20 21:45:16 +0100

    added missing catch to main.js fetch operation. modified render function to handle errors

--- public/main.js
+++ public/main.js
@@ -3,6 +3,10 @@
   const whoami = document.getElementById('whoami');
 
   function render(data) {
+    if (data.error) {
+      return `<p class='response-para'>Error: <span class='response-text'>${data.error}</span></p>
+      `;
+    }
     return `
       <p class='response-para'>IP address: <span class='response-text'>${data.ipaddress}</span></p>
       <p class='response-para'>language: <span class='response-text'>${data.language}</span></p>
@@ -15,6 +19,10 @@
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
+        apiBox.innerHTML = render(data);
+      })
+      .catch((err) => {
+        const data = { error: 'Unable to reach the API server.' };
         apiBox.innerHTML = render(data);
       });
   });
